845c0e1aa3851c0c1fb66a8f8828c65925d7db30
Safiullah Omar <omar.safiullah@gmail.com>
2022-04-07 02:38:04 +0430

    Added Styles and Service

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { QuestionService } from '../service/question.service';
 
 @Component({
   selector: 'app-question',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionComponent implements OnInit {
 
-  constructor() { }
+  public name:string ="";
+  constructor(private QuestionService:QuestionService) { }
 
   ngOnInit(): void {
+
+    this.name=localStorage.getItem("name")!;
+  }
+
+  getAllQuestions(){
+      this.QuestionService.getQuestionJson()
+      .subscribe(res=>{
+        console.log(res);
+      });
+
   }
 
 }
